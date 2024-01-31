b1eaa49f857fda22011578e9df3e82cd55c8abc5
Mysdir <107135687+Mysdir@users.noreply.github.com>
2024-01-31 21:00:53 +0530

    Update llama2-local.py

--- llama2-local.py
+++ llama2-local.py
@@ -1,32 +1,7 @@
-from llama_cpp import Llama 
-import timeit
+import wget
 
-# Load Llama 2 model
-llm = Llama(model_path="llama-2-7b-chat.ggmlv3.q2_K.bin",
-            n_ctx=512,
-            n_batch=128)
+def bar_custom(current, total, width=80):
+    print("Downloading %d%% [%d / %d] bytes" % (current / total * 100, current, total))
 
-# Start timer
-start = timeit.default_timer()
-
-# Generate LLM response
-prompt = "What is Python?"
-
-output = llm(prompt,
-             max_tokens=-1,
-             echo=False,
-             temperature=0.1,
-             top_p=0.9)
-
-# Stop timer
-stop = timeit.default_timer()
-duration = stop - start
-print("Time: ", duration, '\n\n')
-
-# Display generated text
-print(output['choices'][0]['text'])
-
-# Write to file
-with open("response.txt", "a") as f:
-  f.write(f"Time: {duration}")
-  f.write(output['choices'][0]['text'])
+model_url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf"
+wget.download(model_url, bar=bar_custom)
